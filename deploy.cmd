b4eb8cf2c734b6c14ac4f3bcc84ff7be9473a7ec
MikaPaakki <mika.paakki@student.saimia.fi>
2017-12-13 12:49:30 +0200

    Regenerated deployment script

--- deploy.cmd
+++ deploy.cmd
@@ -89,21 +89,15 @@ goto :EOF
 echo Handling node.js deployment.
 
 :: 1. KuduSync
-echo KuduSync begings
-
 IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
   call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
   IF !ERRORLEVEL! NEQ 0 goto error
 )
 
 :: 2. Select node version
-echo SelectNodeVersion begins
-
 call :SelectNodeVersion
 
 :: 3. Install npm packages
-echo install npm packages begins
-
 IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
   pushd "%DEPLOYMENT_TARGET%"
   call :ExecuteCmd !NPM_CMD! install
@@ -123,6 +117,7 @@ IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
   IF !ERRORLEVEL! NEQ 0 goto error
   popd
 )
+
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 goto end
